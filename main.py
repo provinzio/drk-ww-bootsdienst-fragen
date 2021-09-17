c757100f0d087bf41b0a086b57f90a94eb8d50d2
Jeppy <carsten.docktor@gmail.com>
2021-09-17 15:06:35 +0200

    UPDATE Code cleaned, added comments

--- main.py
+++ main.py
@@ -7,9 +7,29 @@ from xml.sax.saxutils import escape
 from tkinter import Tk
 
 
+CREATE_RANDOMIZED_TXT = False
+SAVE_QUESTIONS_FOR_SAILTRAINER = False
+
+
 class Question:
-    def __init__(self, question: str) -> None:
+    """Eine Question besteht aus einer Frage und einer Liste von Antworten.
+
+    Die erste Antwort in der Liste ist die korrekte Antwort.
+    Grundsätzlich hat eine Frage vier Antworten.
+    """
+
+    def __init__(self, question: str, identifier: int, no: int) -> None:
+        """Initialisiere eine Frage.
+
+        Args:
+            question (str): Fragestellung.
+            identifier (int): Eindeutige Nummer der Frage.
+            no (int): Nummer der Frage innerhalb eines Topics.
+        """
         self.question = question
+        self.identifier = identifier
+        self.no = no
+        self.has_picture = False
         # Erste Antwort muss korrekt sein.
         self.answers: list[str] = []
 
@@ -21,6 +41,8 @@ class Question:
 
 
 class Topic:
+    """Ein Topic besteht aus einer Liste von Fragen."""
+
     def __init__(self) -> None:
         self.questions: list[Question] = []
 
@@ -28,6 +50,21 @@ class Topic:
         self.questions.append(question)
 
 
+def xshuffle(items: list) -> list:
+    """Gebe eine Kopie einer Liste zurück, die zufällig sortiert ist.
+
+    Args:
+        items (list): Liste.
+
+    Returns:
+        list: Kopie einer zufällig sortierten Liste.
+    """
+    shuffled_items = items.copy()
+    random.shuffle(shuffled_items)
+    return shuffled_items
+
+
+## Lese die Topics und Fragen ein.
 questions: list[Question] = []
 topics: dict[str, list[Question]] = collections.defaultdict(list)
 
@@ -37,34 +74,34 @@ with open("data/20190127_DRK-WW_BD_Fragenkatalog.pdf.txt", "r", encoding="utf8")
     answer_buffer: list[str] = []
 
     for line in f:
+        # Bereinige die Zeile
         line = line.removeprefix("\ufeff").replace("\t", " ").replace("  ", " ").strip()
 
+        # Prüfe, ob die Zeile ein neues Topic einleitet.
         if m := re.match(r"^\# \d+ (.*)$", line):
-            # if "Regional" in line:
-            #     break
-            # Abschnitt.
             current_topic = m.group(1)
             continue
 
-        if m := re.match(r"^\d+ \(\d+\) (.*)", line):
-
+        # Prüfe, ob die Zeile eine neue Frage einleitet.
+        if m := re.match(r"(^\d+) \((\d+)\) (.*)", line):
             if question is not None:
+                # Es gab vorher schon eine Frage.
+                # Speichere die letzte Frage in der Fragenliste.
                 if answer_buffer:
-                    # Neue Frage, vorherige Antwort speichern.
+                    # Neue Frage, vorheriger Antwort speichern.
                     question.addAnswer(" :: ".join(answer_buffer))
                     # Antwort-Buffer leeren.
                     answer_buffer = []
-                # vorherige Frage speichern.
+                # Vorherige Frage speichern.
                 assert (
                     len(question.answers) == 4
                 ), f"Es sind nicht genau vier Antwortmöglichkeiten: {question.question}"
                 questions.append(question)
                 topics[current_topic].append(question)
-                # print(question)
 
-            # Neue Frage
-            question = Question(m.group(1))
-            # question = Question(line)
+            # Lege die neue Frage an.
+            identifier, no, question_text = m.groups()
+            question = Question(question_text, int(identifier), int(no))
             continue
 
         assert isinstance(
@@ -72,9 +109,10 @@ with open("data/20190127_DRK-WW_BD_Fragenkatalog.pdf.txt", "r", encoding="utf8")
         ), f"Es muss bereits eine Frage geben, {question.__class__.__name__}"
 
         if line == "BILD FEHLT":
-            # question.question += line
+            question.has_picture = True
             continue
 
+        # Prüfe, ob die Frage eine neue Antwort beinhaltet.
         if m := re.match(r"□ [ABCD] (.*)", line):
             if answer_buffer:
                 # Neue Antwort, vorherige Antwort speichern.
@@ -82,80 +120,61 @@ with open("data/20190127_DRK-WW_BD_Fragenkatalog.pdf.txt", "r", encoding="utf8")
                 # Antwort-Buffer leeren.
                 answer_buffer = []
 
-            answer_buffer.append(m.group(1))
-        else:
-            assert answer_buffer, f"Es gibt noch keine Antwort zum Puffern. {line}"
-            answer_buffer.append(line)
+            (answer_text,) = m.groups()
+            answer_buffer.append(answer_text)
+            continue
+
+        # Die vorherigen Fälle sind nicht eingetreten.
+        # Die Zeile gehört zur vorherigen Antwort.
+        # Nehme sie mit im Buffer auf.
+        assert answer_buffer, f"Es gibt noch keine Antwort zum Puffern. {line}"
+        answer_buffer.append(line)
 
 
-with open("sailtrainer/drkwwfragen.xml", "w", encoding="utf8") as f:
-    f.writelines(
-        [
-            "<?xml version='1.0' standalone='yes'?>\n",
-            "<questionaire xmlns='http://sportboot.mobi/'>\n",
-        ]
-    )
+if SAVE_QUESTIONS_FOR_SAILTRAINER:
+    with open("sailtrainer/drkwwfragen.xml", "w", encoding="utf8") as f:
+        f.writelines(
+            [
+                "<?xml version='1.0' standalone='yes'?>\n",
+                "<questionaire xmlns='http://sportboot.mobi/'>\n",
+            ]
+        )
 
-    unique_id = 3000
-    question_id = 1
-    for _, (topic, questions) in enumerate(topics.items(), 1):
-        f.writelines([f"<topic id='{unique_id}' name='{escape(topic)}'>\n"])
-        unique_id += 1
-
-        for _, question in enumerate(questions):
-            assert len(question.answers) == 4
-            f.writelines(
-                [
-                    f"  <question id='{unique_id}' reference='{question_id}'>\n",
-                    f"    <text>{escape(question.question)}</text>\n",
-                    f"    <correct>{escape(question.answers[0])}</correct>\n",
-                    f"    <incorrect>{escape(question.answers[1])}</incorrect>\n",
-                    f"    <incorrect>{escape(question.answers[2])}</incorrect>\n",
-                    f"    <incorrect>{escape(question.answers[3])}</incorrect>\n",
-                    "  </question>\n",
-                ]
-            )
+        unique_id = 3000
+        question_id = 1
+        for _, (topic, questions) in enumerate(topics.items(), 1):
+            f.writelines([f"<topic id='{unique_id}' name='{escape(topic)}'>\n"])
             unique_id += 1
-            question_id += 1
 
-        f.writelines(["</topic>\n"])
-    f.writelines(["</questionaire>\n"])
+            for _, question in enumerate(questions):
+                assert len(question.answers) == 4
+                f.writelines(
+                    [
+                        f"  <question id='{unique_id}' reference='{question_id}'>\n",
+                        f"    <text>{escape(question.question)}</text>\n",
+                        f"    <correct>{escape(question.answers[0])}</correct>\n",
+                        f"    <incorrect>{escape(question.answers[1])}</incorrect>\n",
+                        f"    <incorrect>{escape(question.answers[2])}</incorrect>\n",
+                        f"    <incorrect>{escape(question.answers[3])}</incorrect>\n",
+                        "  </question>\n",
+                    ]
+                )
+                unique_id += 1
+                question_id += 1
 
+            f.writelines(["</topic>\n"])
+        f.writelines(["</questionaire>\n"])
 
-# XX = "\n".join(
-#     "\t".join(
-#         (
-#             str(i),
-#             q.question,
-#             q.answers[0],
-#             "1",
-#             q.answers[1],
-#             "",
-#             q.answers[2],
-#             "",
-#             q.answers[3],
-#         )
-#     )
-#     for i, q in enumerate(questions)
-# )
-
-# with open("output.txt", "w", encoding="utf8") as f:
-#     f.write(XX)
-
-
-# def xshuffle(l: list) -> list:
-#     x = l.copy()
-#     random.shuffle(x)
-#     return x
-
-
-# for i in range(5):
-#     random.shuffle(questions)
-#     with open(f"random-{i}.txt", "w", encoding="utf8") as f:
-#         f.write(
-#             "\n\n".join(
-#                 f"{q.question}\n"
-#                 + "\n".join(f"{i}) {a}" for i, a in enumerate(xshuffle(q.answers), 1))
-#                 for q in questions
-#             )
-#         )
+if CREATE_RANDOMIZED_TXT:
+    for i in range(5):
+        random.shuffle(questions)
+        with open(f"random-{i}.txt", "w", encoding="utf8") as f:
+            f.write(
+                "\n\n".join(
+                    f"{q.question}\n"
+                    + "\n".join(
+                        f"{i}) {a}" for i, a in enumerate(xshuffle(q.answers), 1)
+                    )
+                    for q in questions
+                )
+            )
